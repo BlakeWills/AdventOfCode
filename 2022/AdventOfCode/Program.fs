1e0b35e21a3bc34f785795f9e4e737dc184ad664
Blake <blakewills@outlook.com>
2022-12-03 21:23:33 +0000

    dispose stream

--- 2022/AdventOfCode/Program.fs
+++ 2022/AdventOfCode/Program.fs
@@ -8,32 +8,33 @@ let path =
     |> List.filter (fun x -> File.Exists (Path.Combine (Environment.CurrentDirectory, x)))
     |> List.head
 
-let stream = new StreamReader (path)
+let calculateCalories file = 
+    use stream = new StreamReader (path)
 
-let mutable elfCalories = List.empty;
-let mutable curCalories = 0
+    let mutable elfCalories = List.empty;
+    let mutable curCalories = 0
 
-let incCalories v =
-    let cal = v |> int
-    curCalories <- curCalories + cal
+    let incCalories v =
+        let cal = v |> int
+        curCalories <- curCalories + cal
 
-let newElf () =
-    elfCalories <- List.append elfCalories [curCalories]
-    curCalories <- 0
+    let newElf () =
+        elfCalories <- List.append elfCalories [curCalories]
+        curCalories <- 0
 
-let mutable moreData = true
-while moreData do
-    let line = stream.ReadLine()
-    match line with
-    | "" -> newElf() |> ignore
-    | null -> moreData <- false
-    | v -> incCalories v
+    let mutable moreData = true
+    while moreData do
+        let line = stream.ReadLine()
+        match line with
+        | "" -> newElf() |> ignore
+        | null -> moreData <- false
+        | v -> incCalories v
 
-let topThreeCalories =
     elfCalories
-    |> List.sort
-    |> List.skip (elfCalories.Length - 3)
-    |> List.sum
+        |> List.sort
+        |> List.skip (elfCalories.Length - 3)
+        |> List.sum
 
-printfn $"Top three elves are carrying {topThreeCalories} calories"
+let calories = calculateCalories path
+printfn $"Top three elves are carrying {calories} calories"
 
